42928f4ea2c8d3289864295af9557a519b11f6c4
Madstrawberry <a.engelse@gmail.com>
2020-02-24 22:08:35 +0100

    Add normalize

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 export const GlobalStyles = createGlobalStyle`
+  ${normalize}
+  
   body {
     background: ${props => props.theme.palette.background};
-    margin: 0;
     padding: 0;
     font-family: 'Simonetta', 'Helvetica Neue', 'Arial';
     -webkit-font-smoothing: antialiased;
