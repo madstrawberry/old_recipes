a6fe4377b8dc76ac35133501e4768399c93e9437
Madstrawberry <a.engelse@gmail.com>
2020-02-24 22:43:29 +0100

    remove unused css

--- src/components/shared/Label.tsx
+++ src/components/shared/Label.tsx
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
 
 export const Label = styled.div<{ type: string }>`
-  padding: 3px;
+  padding: ${props => props.theme.grid.xs}px;
   font-size: 13px;
   text-transform: uppercase;
   font-weight: ${props => props.theme.font.bold};
   color: #fff;
   border-radius: 3px;
   background: pink;
-
-  & + & {
-    margin-left: ${props => props.theme.grid.sm}px;
-  }
 `;
