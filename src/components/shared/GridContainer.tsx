ca6ee0fe514aab7527a2662bd82d0cf984b18238
Madstrawberry <a.engelse@gmail.com>
2020-03-10 22:47:51 +0100

    Add responsive columnGap

--- src/components/shared/GridContainer.tsx
+++ src/components/shared/GridContainer.tsx
@@ -2,10 +2,11 @@ import styled, { css } from 'styled-components';
 import { GridColumn } from './GridColumn';
 import { GridSize } from '../../styles/theme';
 import { gridInPx, grid } from '../../styles/themeHelpers';
+import { fromDevice } from '../../styles/mediaQueries';
 
 type GridContainerProps = {
   alignItems?: 'center' | 'baseline';
-  columnGap?: GridSize;
+  columnGap?: GridSize | GridSize[];
   top?: GridSize;
 };
 
@@ -13,16 +14,85 @@ export const GridContainer = styled.div<GridContainerProps>`
   display: flex;
   flex-wrap: wrap;
   ${props => props.alignItems && `align-items: ${props.alignItems}`};
-  ${props => props.columnGap && columnGap};
-  ${props => props.top && `margin-top: ${gridInPx(props, props.top)}`}
+  ${props => columnGapDefault(getDefaultGap(props.columnGap))};
+  ${props => columnGapResponsive(getResponsiveGaps(props.columnGap))};
+  ${props => marginTopDefault(props.top, getDefaultGap(props.columnGap))};
+  ${props => marginTopResponsive(props.top, getResponsiveGaps(props.columnGap))};
 `;
 
-const columnGap = css<GridContainerProps>`
-  margin-left: -${props => grid(props, props.columnGap!) / 2}px;
-  margin-right: -${props => grid(props, props.columnGap!) / 2}px;
+function getDefaultGap(columnGap?: GridContainerProps['columnGap']): GridSize | undefined {
+  if (!columnGap) return undefined;
+  if (!Array.isArray(columnGap)) return columnGap;
+  if (Array.isArray(columnGap)) return columnGap[0];
+}
 
-  > ${GridColumn} {
-    padding-left: ${props => grid(props, props.columnGap!) / 2}px;
-    padding-right: ${props => grid(props, props.columnGap!) / 2}px;
-  }
-`;
+function getResponsiveGaps(columnGap?: GridContainerProps['columnGap']): GridSize[] | undefined {
+  if (!columnGap || !Array.isArray(columnGap) || columnGap.length === 1) return undefined;
+  return columnGap;
+}
+
+const marginTopDefault = (top?: GridSize, columnGap?: GridSize) => {
+  if (!top && !columnGap) return;
+
+  return css<GridContainerProps>`
+    margin-top: ${props => {
+      const topSize = top ? grid(props, top) : 0;
+      const gapSize = columnGap ? grid(props, columnGap) : 0;
+
+      return `${topSize - gapSize}px`;
+    }};
+  `;
+};
+
+const marginTopResponsive = (top?: GridSize, columnGaps?: GridSize[]) => {
+  if (!columnGaps) return;
+
+  return css<GridContainerProps>`
+    ${props =>
+      columnGaps.slice(1).map((gap, index) => {
+        const topSize = top ? grid(props, top) : 0;
+        const gapSize = grid(props, gap);
+
+        return `${Object.values(fromDevice)[index]} {
+            margin-top: ${topSize - gapSize}px;  
+          }`;
+      })}
+  `;
+};
+
+const columnGapDefault = (columnGap?: GridSize) => {
+  if (!columnGap) return;
+
+  return css<GridContainerProps>`
+    margin-left: -${props => grid(props, columnGap) / 2}px;
+    margin-right: -${props => grid(props, columnGap) / 2}px;
+
+    > ${GridColumn} {
+      padding-left: ${props => grid(props, columnGap) / 2}px;
+      padding-right: ${props => grid(props, columnGap) / 2}px;
+      padding-top: ${props => gridInPx(props, columnGap)};
+    }
+  `;
+};
+
+const columnGapResponsive = (columnGaps?: GridSize[]) => {
+  if (!columnGaps) return;
+
+  return css<GridContainerProps>`
+    ${props =>
+      columnGaps.slice(1).map(
+        (gap, index) =>
+          `${Object.values(fromDevice)[index]} {
+          margin-left: -${grid(props, gap) / 2}px;
+          margin-right: -${grid(props, gap) / 2}px;
+        
+          > ${GridColumn} {
+            padding-left: ${grid(props, gap) / 2}px;
+            padding-right: ${grid(props, gap) / 2}px;
+            padding-top: ${gridInPx(props, gap)};
+          }
+        }
+        `
+      )}
+  `;
+};
